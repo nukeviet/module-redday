51cfe3329b249a115cc1dbdcb27a00a44f153c1e
Hoang Quang <quangpro1610@gmail.com>
2024-03-12 16:11:56 +0700

    Thêm lang cho EN (#10)

---
+++ modules/redday/language/admin_en.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_ADMIN') or !defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'VINADES.,JSC (contact@vinades.vn)';
+$lang_translator['createdate'] = '04/03/2010, 15:22';
+$lang_translator['copyright'] = '@Copyright (C) 2012 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['all'] = 'All';
+$lang_module['add'] = 'Add';
+$lang_module['close'] = 'Close';
+$lang_module['title'] = 'Title';
+$lang_module['alias'] = 'Alias';
+$lang_module['errorsave'] = 'Error: The data can not saved';
+$lang_module['function'] = 'Function';
+$lang_module['order'] = 'Order';
+$lang_module['status'] = 'Status';
+$lang_module['status1'] = 'Activated';
+$lang_module['keywords'] = 'Keywords';
+$lang_module['search_keywords'] = 'Search keywords';
+$lang_module['description'] = 'Description';
+$lang_module['illustrating_images'] = 'Illustrating images';
+$lang_module['note'] = 'Note';
+$lang_module['enter_search_key'] = 'Enter keywords';
+$lang_module['select2_pick'] = 'Enter keywords to search and select';
+$lang_module['addtime'] = 'Create at';
+$lang_module['edittime'] = 'Updated at';
+$lang_module['approval'] = 'Approval';
+$lang_module['msgnocheck'] = 'Please select at least one line to execute';
+$lang_module['to'] = 'To';
+$lang_module['from_day'] = 'From day';
+$lang_module['to_day'] = 'To day';
+$lang_module['is_required'] = 'Required';
+$lang_module['tools'] = 'Tools';
+
+$lang_module['main'] = 'Event list';
+$lang_module['main_add'] = 'Add event';
+$lang_module['main_edit'] = 'Edit event';
+$lang_module['main_cat'] = 'Belongs to the category';
+$lang_module['main_cat1'] = 'Category';
+$lang_module['main_cat2'] = 'Main category';
+$lang_module['main_bodyhtml'] = 'Event content';
+$lang_module['main_error_catids'] = 'No category selected';
+$lang_module['main_error_catid'] = 'Please select the main category';
+$lang_module['main_error_title'] = 'The title is not empty';
+$lang_module['main_error_exists'] = 'The alias is duplicated, please enter another';
+$lang_module['main_error_bodyhtml'] = 'The content is not empty';
+$lang_module['main_error_exits_cat'] = 'No category exists';
+$lang_module['main_error_month_match'] = 'The incorrect month';
+$lang_module['main_error_day_match'] = 'The incorrect day';
+$lang_module['main_search_from'] = 'Created from date';
+$lang_module['main_search_to'] = 'Created up to date';
+$lang_module['main_search_d'] = 'Event on date';
+$lang_module['main_search_m'] = 'Event on month';
+
+$lang_module['excel_export_html'] = 'Export excel events as full HTML code';
+$lang_module['excel_export_plaintext'] = 'Export excel events as plain text';
+$lang_module['excel_import'] = 'Import events from excel file';
+$lang_module['excel_import_alt'] = 'Import from excel';
+$lang_module['excel_download_template'] = 'Download example';
+$lang_module['excel_note_template'] = 'Click the button below to download the excel template, enter data according to that template. It is required not to add, remove, or change the order of columns';
+$lang_module['excel_file_label'] = 'Select the excel file after filling in all the lines';
+$lang_module['excel_pickcat'] = 'Import to category';
+$lang_module['excel_skipcat'] = 'Do not update the list of existing data';
+$lang_module['excel_truncate'] = 'Delete all category events before importing';
+$lang_module['excel_error_nofile'] = 'The excel file has not been selected';
+$lang_module['import_error_readexcel'] = 'Unable to read file, check excel format again';
+$lang_module['excel_skip_error'] = 'Ignore error lines';
+$lang_module['excel_nl2br'] = 'Convert line breaks in Excel to line breaks in HTML';
+$lang_module['excel_error_time'] = 'Invalid date at line';
+$lang_module['excel_error_content'] = 'Empty content at line';
+$lang_module['excel_success'] = 'Import was successful, information is as follows';
+$lang_module['excel_success_add'] = 'Number of newly added events';
+$lang_module['excel_success_update'] = 'Number of updated events';
+$lang_module['excel_success_skip'] = 'Line numbers are ignored';
+
+$lang_module['cat'] = 'Topic';
+$lang_module['add_content'] = 'Add event';
+$lang_module['cat'] = 'Category';
+$lang_module['cat_title'] = 'Category name';
+$lang_module['cat_description'] = 'Category description';
+$lang_module['cat_add'] = 'Add categories';
+$lang_module['cat_edit'] = 'Edit category';
+$lang_module['cat_error_title'] = 'Can not leave the title blank';
+$lang_module['cat_error_exists'] = 'The title is exists';
+
+$lang_module['main'] = 'Day in the past';
+$lang_module['rdselectday'] = 'Select date';
+$lang_module['rdselectmonth'] = 'Select month';
+$lang_module['rdsubmit'] = 'Show';
+$lang_module['rderrdate'] = 'Please select the date to edit';
+$lang_module['rdeditday'] = 'Edit record date %s month %s';
+$lang_module['rdeditmonth'] = 'month';
+$lang_module['rdholydays'] = 'National holiday';
+$lang_module['stateevents'] = 'Domestic events';
+$lang_module['interevents'] = 'Overseas events';
+$lang_module['otherevents'] = 'Other events';
+$lang_module['reddayadmin'] = 'Day in the past';
+$lang_module['notauthorized'] = 'You do not have permission to access this area';
+$lang_module['save_ok'] = 'Save data successfully!';
+$lang_module['redirect_to_home'] = 'Click to return to the home page';
+$lang_module['redday_time'] = 'Events of day %s month %s years ago';
+
+$lang_module['error_month2'] = 'Error: February does not have a day %s';
+$lang_module['error_month'] = 'Error: Month %s does not have day %s';
---
+++ modules/redday/language/en.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'VINADES.,JSC (contact@vinades.vn)';
+$lang_translator['createdate'] = '04/03/2010, 15:22';
+$lang_translator['copyright'] = '@Copyright (C) 2012 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['op_day'] = 'day';
+$lang_module['op_month'] = 'mont';
+
+$lang_module['main_title'] = 'Day in the past';
+$lang_module['main_title_redday'] = 'Date %s month %s year ago';
+$lang_module['main_description_redday'] = 'Holidays and events at home and abroad that took place on day %s, month %s of the past year';
+$lang_module['rdselectday'] = 'Select date';
+$lang_module['rdselectmonth'] = 'Select month';
+$lang_module['rdsubmit'] = 'Show';
+$lang_module['rdday'] = 'Day';
+$lang_module['rdmonth'] = 'Month';
